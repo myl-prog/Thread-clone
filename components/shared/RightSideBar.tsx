c89ef3e0ebbb65a360411c12f40cf68e89755b3e
yassine <mohamedyassinelguirati@gmail.com>
2023-08-15 23:20:23 +0200

    Full layout with RightSidebar template

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -1,4 +1,30 @@
-function RightSideBar() {
-    return <h1>RightSidebar</h1>
+import { currentUser } from "@clerk/nextjs";
+
+
+
+async function RightSidebar() {
+  
+
+  return (
+    <section className='custom-scrollbar rightsidebar'>
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>
+          Suggested Communities
+        </h3>
+
+        <div className='mt-7 flex w-[350px] flex-col gap-9'>
+          
+        </div>
+      </div>
+
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>Similar Minds</h3>
+        <div className='mt-7 flex w-[350px] flex-col gap-10'>
+          
+        </div>
+      </div>
+    </section>
+  );
 }
-export default RightSideBar;
+
+export default RightSidebar;
